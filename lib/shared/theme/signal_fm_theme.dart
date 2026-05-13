acc365003dba460a9e7b06df24f4dc63c1b29849
EcoMetric-arch <ecometric@proton.me>
2026-05-14 00:10:22 +0200

    Update signal_fm_theme.dart

--- lib/shared/theme/signal_fm_theme.dart
+++ lib/shared/theme/signal_fm_theme.dart
@@ -52,7 +52,7 @@ class MarketStateData {
   final String aiDjMessage;
 }
 
-class MarketStates {
+class SignalFmSessions {
   static const MarketStateData calm = MarketStateData(
     label: 'Calm',
     glowScale: 0.6,
